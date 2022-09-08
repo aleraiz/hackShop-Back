d1700700acc00ad94967d83d68a44685a7df4c02
antondl <marcoanton.deluca@gmail.com>
2022-09-08 20:10:08 -0300

    Cabmios

--- dbInitialSetUp.js
+++ dbInitialSetUp.js
@@ -1,20 +1,19 @@
-const db = require("./models/Index");
+const sequelize = require("./models/Index");
 
-// const productSeeder = require("./seeders/productSeeder");
-// const categorySeeder = require("./seeders/categorySeeder");
-// const orderSeeder = require("./seeders/orderSeeder");
-// const purchaseHistorySeeder = require("./seeders/purchaseHistorySeeder");
-const buyerSeeder = require("./seeders/buyerSeeder");
 const administratorSeeder = require("./seeders/administratorSeeder");
+const productSeeder = require("./seeders/productSeeder");
+const categorySeeder = require("./seeders/categorySeeder");
+const orderSeeder = require("./seeders/orderSeeder");
+const purchaseHistorySeeder = require("./seeders/purchaseHistorySeeder");
+const buyerSeeder = require("./seeders/buyerSeeder");
 
 module.exports = async function () {
-  await db.sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
   await administratorSeeder();
   await buyerSeeder();
-  // await categorySeeder();
-  // await orderSeeder();
-  // await productSeeder();
-  // await purchaseHistorySeeder();
-
+  await categorySeeder();
+  await orderSeeder();
+  await productSeeder();
+  await purchaseHistorySeeder();
   console.log("Se crearon las tablas");
 };
