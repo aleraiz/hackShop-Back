9c680e2399c3c003aa22127382d9d9e0e1f17f10
antondl <marcoanton.deluca@gmail.com>
2022-09-14 16:15:39 -0300

    Arreglo models y seeder de Client

--- seeders/clientSeeder.js
+++ seeders/clientSeeder.js
@@ -1,41 +1,13 @@
 const { Client } = require("../models");
 
 module.exports = async function () {
-  const client = [
-    {
-      firstname: "Juan",
-      lastname: "Sanchez",
-      email: "juansanchez@gmail.com",
-      password: "123",
-      address: [],
-      phoneNumber: "13215468",
-    },
-    {
-      firstname: "User",
-      lastname: "User",
-      email: "user@user.com",
-      password: "123",
-      address: [],
-      phoneNumber: "13215468",
-    },
-
-    {
-      firstname: "Roberto",
-      lastname: "Rodriguez",
-      email: "robertorodriguez@gmail.com",
-      password: "123",
-      address: [],
-      phoneNumber: "2213215468",
-    },
-
-    {
-      firstname: "Matias",
-      lastname: "Martinez",
-      email: "matiasmartinez@gmail.com",
-      password: "123",
-      address: [],
-      phoneNumber: "132154638",
-    },
-  ];
-  await Client.bulkCreate(client);
+  const client = {
+    firstname: "User",
+    lastname: "User",
+    email: "user@user.com",
+    password: "123",
+    address: [],
+    phoneNumber: "13215468",
+  };
+  await Client.create(client);
 };
